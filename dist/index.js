23aa4758b39fa513c888b7692acb5489c8e5d7c4
Samuel Steele <steelesam72@gmail.com>
2022-03-22 09:59:36 -0400

    rebuild

--- dist/index.js
+++ dist/index.js
@@ -8721,9 +8721,17 @@ AbortError.prototype = Object.create(Error.prototype);
 AbortError.prototype.constructor = AbortError;
 AbortError.prototype.name = 'AbortError';
 
+const URL$1 = Url.URL || whatwgUrl.URL;
+
 // fix an issue where "PassThrough", "resolve" aren't a named export for node <10
 const PassThrough$1 = Stream.PassThrough;
-const resolve_url = Url.resolve;
+
+const isDomainOrSubdomain = function isDomainOrSubdomain(destination, original) {
+	const orig = new URL$1(original).hostname;
+	const dest = new URL$1(destination).hostname;
+
+	return orig === dest || orig[orig.length - dest.length - 1] === '.' && orig.endsWith(dest);
+};
 
 /**
  * Fetch function
@@ -8811,7 +8819,19 @@ function fetch(url, opts) {
 				const location = headers.get('Location');
 
 				// HTTP fetch step 5.3
-				const locationURL = location === null ? null : resolve_url(request.url, location);
+				let locationURL = null;
+				try {
+					locationURL = location === null ? null : new URL$1(location, request.url).toString();
+				} catch (err) {
+					// error here can only be invalid URL in Location: header
+					// do not throw when options.redirect == manual
+					// let the user extract the errorneous redirect URL
+					if (request.redirect !== 'manual') {
+						reject(new FetchError(`uri requested responds with an invalid redirect URL: ${location}`, 'invalid-redirect'));
+						finalize();
+						return;
+					}
+				}
 
 				// HTTP fetch step 5.5
 				switch (request.redirect) {
@@ -8858,6 +8878,12 @@ function fetch(url, opts) {
 							timeout: request.timeout,
 							size: request.size
 						};
+
+						if (!isDomainOrSubdomain(request.url, locationURL)) {
+							for (const name of ['authorization', 'www-authenticate', 'cookie', 'cookie2']) {
+								requestOpts.headers.delete(name);
+							}
+						}
 
 						// HTTP-redirect fetch step 9
 						if (res.statusCode !== 303 && request.body && getTotalBytes(request) === null) {
@@ -18244,9 +18270,28 @@ async function action(payload) {
 
 function formatFileUrl(sourceDir, fileName, commit) {
   const repo = github.context.repo;
+
+  // TODO: support mapping root via index of colon delimited list in `sourceDir`
+  // strip deterministic roots (see: https://github.com/danielpalme/ReportGenerator/issues/405)
+  fileName = fileName.replace(/^(\/{0,1}_\d{0,}\/)/, "");
   sourceDir = sourceDir ? sourceDir : "";
+
   // Strip leading and trailing slashes.
   sourceDir = sourceDir.replace(/\/$/, "").replace(/^\//, "");
+
+  core.debug(
+    `formatFileUrl: normalized inputs (fileName: ${fileName}, sourceDir: ${sourceDir})`
+  );
+
+  // check if the file is a raw github asset (e.g. coverlet UseSourceLink==true, see: https://github.com/coverlet-coverage/coverlet/blob/1f1a10143517da4e7b48b50caf16b407abe5a233/Documentation/VSTestIntegration.md#advanced-options-supported-via-runsettings)
+  var rawContentMatch = new RegExp(
+    `^(http|https):\/\/raw\.githubusercontent\.com\/${repo.owner}\/${repo.repo}\/${commit}`
+  ).exec(fileName);
+  if (!!rawContentMatch) {
+    core.debug(`formatFileUrl: detected GitHub Raw content '${fileName}'`);
+    fileName = fileName.replace(rawContentMatch[0], "").replace(/^\//, "");
+  }
+
   const path = (sourceDir ? `${sourceDir}/` : "") + fileName;
   return `https://github.com/${repo.owner}/${repo.repo}/blob/${commit}/${path}`;
 }
